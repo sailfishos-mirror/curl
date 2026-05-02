37b2403f48959c632c8dd1dac688087237410ea2
Daniel Stenberg <daniel@haxx.se>
2026-05-02 22:50:10 +0200

    lib: drop support for CURLAUTH_DIGEST_IE
    This bit was used to do Digest authentication like Internet Explorer
    before version 7 (released on October 18, 2006). Presumably no one uses
    this anymore and since it is hard to use and does broken auth, starting
    in 8.21.0 this bit does nothing (except setting the actual Digest bit).
    
    Closes #21486

--- lib/setopt.c
+++ lib/setopt.c
@@ -240,17 +240,9 @@ static CURLcode httpauth(struct Curl_easy *data, bool proxy,
   if(auth != CURLAUTH_NONE) {
     int bitcheck = 0;
     bool authbits = FALSE;
-    /* the DIGEST_IE bit is only used to set a special marker, for all the
-       rest we need to handle it as normal DIGEST */
-    bool iestyle = !!(auth & CURLAUTH_DIGEST_IE);
-    if(proxy)
-      data->state.authproxy.iestyle = iestyle;
-    else
-      data->state.authhost.iestyle = iestyle;
-
     if(auth & CURLAUTH_DIGEST_IE) {
       auth |= CURLAUTH_DIGEST; /* set standard digest bit */
-      auth &= ~CURLAUTH_DIGEST_IE; /* unset ie digest bit */
+      auth &= ~CURLAUTH_DIGEST_IE; /* drop the legacy bit */
     }
 
     /* switch off bits we cannot support */
