71a5725563f21bfd52a4f47312a603f1f69f5609
Stefan Eissing <stefan@eissing.org>
2026-05-06 09:49:14 +0200

    ftp: remove 2 Curl_resolv_blocking() calls
    They are no longer needed with the new peers and dns filter.
    Connection setup will take care of the resoling and connecting.
    
    Closes #21512

--- lib/connect.h
+++ lib/connect.h
@@ -116,12 +116,11 @@ CURLcode Curl_cf_setup_insert_after(struct Curl_cfilter *cf_at,
 /**
  * Setup the cfilters at `sockindex` in connection `conn`.
  * If no filter chain is installed yet, inspects the configuration
- * in `data` and `conn? to install a suitable filter chain.
+ * in `data` and `conn` to install a suitable filter chain.
  */
 CURLcode Curl_conn_setup(struct Curl_easy *data,
                          struct connectdata *conn,
                          int sockindex,
-                         struct Curl_dns_entry *dns,
                          int ssl_mode);
 
 /* Set conn to allow multiplexing. */
