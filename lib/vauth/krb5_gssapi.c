5e99b73cf441d9c369768b9cd48b5389b9a2503d
Stefan Eissing <stefan@eissing.org>
2026-05-13 12:02:48 +0200

    creds: add sasl service name
    The SASL service name, used in authentication, is part of curl's credentials
    when authenticating to a server/proxy. Make it part of `struct Curl_creds`.
    
    Change code to use `creds` to obtain a service name. By tying creds used
    to the connection, connection reuse is also only allowed when the service
    name matches.
    
    Closes #21585

--- lib/vauth/krb5_gssapi.c
+++ lib/vauth/krb5_gssapi.c
@@ -75,7 +75,7 @@ bool Curl_auth_is_gssapi_supported(void)
  */
 CURLcode Curl_auth_create_gssapi_user_message(struct Curl_easy *data,
                                               struct Curl_creds *creds,
-                                              const char *service,
+                                              const char *default_service,
                                               const char *host,
                                               const bool mutual_auth,
                                               const struct bufref *chlg,
@@ -88,8 +88,8 @@ CURLcode Curl_auth_create_gssapi_user_message(struct Curl_easy *data,
   OM_uint32 unused_status;
   gss_buffer_desc input_token = GSS_C_EMPTY_BUFFER;
   gss_buffer_desc output_token = GSS_C_EMPTY_BUFFER;
-
-  (void)creds;
+  const char *service = Curl_creds_has_sasl_service(creds) ?
+    Curl_creds_sasl_service(creds) : default_service;
 
   if(!krb5->spn) {
     gss_buffer_desc spn_token = GSS_C_EMPTY_BUFFER;
